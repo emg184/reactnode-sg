ffed392d5c2a080fc5dab045cc4d4348782f5056
emg184 <gardner.ethan@comcast.net>
2017-09-08 21:05:15 -0400

    tweak google strategy settings

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,4 @@
 module.exports = {
-  //googleClientID: '109323302830-0ekhhegh9gn9pgalcmp1jb9rspjgccqd.apps.googleusercontent.com',
-  //googleClientSecret: 'rBwchPHXDj8u0yZHQZ3lGa1S',
-  //mongoURI: 'mongodb://ethanprod:manofsteel@1@ds127994.mlab.com:27994/eg-emaily-prod',
-  //cookieKey: 'asdfasdfasdfasdfasdfasf'
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
